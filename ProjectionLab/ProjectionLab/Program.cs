a2d250df3816c111d46fe31c569ef99f9a214ff6
Andrew Seba <sebaturtle@gmail.com>
2016-02-16 20:03:31 -0600

    Started Testing

--- ProjectionLab/ProjectionLab/Program.cs
+++ ProjectionLab/ProjectionLab/Program.cs
@@ -7,6 +7,16 @@ namespace ProjectionLab
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Projection Lab");
+            Vector3D vector1 = new Vector3D();
+            vector1.SetRectGivenRect(2, 2, 2);
+            Vector3D vector2 = new Vector3D();
+            vector2.SetRectGivenRect(3, 3, 3);
+
+            Console.WriteLine("Add: " + (vector1 + vector2));
+            Console.WriteLine("Subtract: " + (vector1 - vector2));
+            Console.WriteLine("Subtract: " + (vector2 - vector1));
+            Console.WriteLine();
         }
     }
 }
