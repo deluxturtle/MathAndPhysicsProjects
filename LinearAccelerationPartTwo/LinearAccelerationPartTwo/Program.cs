0d5c105eb5a6993f04c21f4bf5962eb357ba3561
Andrew Seba <sebaturtle@gmail.com>
2016-02-22 16:52:57 -0600

    Make dem graphs.

--- LinearAccelerationPartTwo/LinearAccelerationPartTwo/Program.cs
+++ LinearAccelerationPartTwo/LinearAccelerationPartTwo/Program.cs
@@ -1,18 +1,11 @@
 ﻿using System;
 using System.IO;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-//using ProjectionLab;
-using VectorClassLab;
 
-namespace LinearAcceleratedMotionLab
+namespace LinearAccelerationPartTwo
 {
     /// <summary>
-    /// @Author: Andrew Seba
-    /// @Description: Outputs a csv file of physics data of a ball being thrown
-    /// upwards.
+    /// @Author:Andrew Seba
+    /// @Description: Using Equation 5 and 7 output the data of the motion.
     /// </summary>
     class Program
     {
@@ -24,22 +17,25 @@ namespace LinearAcceleratedMotionLab
             float curVelocity = initialVelocity;
             float curY = initialHeight;
 
-            const float gravity = -9.8f;
+            const float acceleration = -9.8f;
 
             float curTime = 0.0f;
             float endTime = 10.00f;
-            float timeStep = 0.01f;
+            float timeStep = 0.10f;
 
-            //
-            using(StreamWriter writer = new StreamWriter("ex02_eular_0-01.csv"))
+
+            using (StreamWriter writer = new StreamWriter("ex02_analytic.csv"))
             {
                 Console.WriteLine("Time(s):\tPosition(m)\tVelocity(m/s)");
                 writer.WriteLine("Time(s),Position(m),Velocity(m/s)");
                 while (curTime <= endTime)
                 {
 
-                    curY += curVelocity * timeStep; //Find the new position.
-                    curVelocity += gravity * timeStep;//Find the new velocity
+                    //Find the new position.
+                    curY += curVelocity * timeStep + (acceleration * (timeStep * timeStep)) / 2.0f;
+                    //Find the new velocity
+                    curVelocity += acceleration * timeStep;
+
                     //Output Time, Position, Velocity data for the ball.
                     Console.WriteLine(string.Format("{0:N}\t{1:N}\t{2:N}", curTime, curY, curVelocity));
                     writer.WriteLine(string.Format("{0:N},{1:N},{2:N}", curTime, curY, curVelocity));
