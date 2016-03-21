bb9854e63f2ef585d3556dc8ffaed0616b3157a9
Andrew Seba <sebaturtle@gmail.com>
2016-03-21 16:26:19 -0500

    started work and energy
    chose option 2

---
+++ WorkAndEnergyLabPart1/WorkAndEnergyLabPart1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkAndEnergyLabPart1
+{
+    /// <summary>
+    /// @Author: Andrew Seba
+    /// @Description: Marble in a bowl option
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
